f26d2b0f1987544d4cb642897e8332679f6a24f6
GitHub Action <action@github.com>
2022-04-08 18:25:51 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/spragucm/intellijpluginprojectviewfolding/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.spragucm.intellijpluginprojectviewfolding.services
+
+import com.github.spragucm.intellijpluginprojectviewfolding.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
